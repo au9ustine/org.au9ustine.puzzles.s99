ac083ceafae58d14edd0de1353f31f8372cebecf
au9ustine <duke.augustine@gmail.com>
2015-10-12 21:45:44 +0800

    add p11

---
+++ src/main/scala/org/au9ustine/puzzles/s99/P11.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 Shao Tian-Chen (Austin)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.au9ustine.puzzles.s99
+
+/**
+ *
+ *   Problem 11: Modified run-length encoding.
+ *
+ *   Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into the
+ *   result list. Only elements with duplicates are transferred as (N, E) terms.
+ *
+ *   Example:
+ *
+ *   scala> encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+ *   res0: List[Any] = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
+ */
+object P11 {
+  def encodeModified[A](lst: List[A]): List[Any] = P10.encode(lst).map { t =>
+    if (t._1 == 1) t._2 else t
+  }
+}
---
+++ src/test/scala/org/au9ustine/puzzles/s99/P11$Test.scala
@@ -0,0 +1,18 @@
+package org.au9ustine.puzzles.s99
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Problem 11 Unit Testing
+ *
+ * Created by shaotch on 10/12/15.
+ */
+class P11$Test extends FlatSpec with Matchers {
+
+  "P10.pack" should "group duplicated word into lists of a list "  in {
+    val lst = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    val expected = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
+    P11.encodeModified(lst) should be (expected)
+  }
+
+}
